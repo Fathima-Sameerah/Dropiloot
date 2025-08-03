b0b996af87dc19c376f7f5bc8d3d89bef0e6b96c
Fathima-Sameerah <sameerahsamuil@gmail.com>
2025-08-03 14:34:44 +0530

    added varify otp

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/auth/send-otp', [AuthController::class, 'sendOtp']);
+Route::post('/auth/verify-otp', [AuthController::class, 'verifyOtp']);
